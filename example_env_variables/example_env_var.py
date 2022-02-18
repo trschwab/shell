c7340bb6e7f0fe057694a99a7d41686791372e76
Troy Schwab <trschwab7@gmail.com>
2022-02-18 14:58:35 -0500

    added example sensitive environment variables

---
+++ example_env_variables/example_env_var.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['SPOTIPY_REDIRECT_URI'] = "https://www.someurl.com/"
+os.environ['SPOTIPY_CLIENT_ID'] = "somestringofcharacters"
+os.environ['SPOTIPY_CLIENT_SECRET'] = "somestringofcharacters"
+
